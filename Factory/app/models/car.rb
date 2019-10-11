ff808df5ad264ee5b17bc16123e690a5695438fa
knifehand <mhurt@msudenver.edu>
2019-10-11 14:15:22 -0600

    Validations for Cars and Parts forms complete

--- Factory/app/models/car.rb
+++ Factory/app/models/car.rb
@@ -1,2 +1,7 @@
 class Car < ApplicationRecord
+  validates :make, :model, :vin, :country, presence: true
+  validates :vin, uniqueness: true
+  #validates :vin, format: {
+  #    with: %r{\.(gif\jpeg\png)\Z}i,
+  #    message: 'must be a URL for GIF,JPG or PNG image.' }
 end
