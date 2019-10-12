4cdce68fb2f052cabe2aac119b2894d2a4368ed4
knifehand <mhurt@msudenver.edu>
2019-10-12 10:01:04 -0600

    changed store index

--- Factory/config/routes.rb
+++ Factory/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
   resources :cars
   resources :parts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
