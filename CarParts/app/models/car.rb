c40359c151b01748b378b822f02053eb3ab1e9d9
knifehand <mhurt@msudenver.edu>
2019-10-26 09:09:14 -0600

    FIXME

--- CarParts/app/models/car.rb
+++ CarParts/app/models/car.rb
@@ -1,2 +1,3 @@
 class Car < ApplicationRecord
+  #has_many :products #FIXME: Need to associate the car with the product.
 end
--- CarParts/app/models/product.rb
+++ CarParts/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  #belongs_to :car  #FIXME: Need to associate product with the car.
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_orequal_to: 0.01}
 end
