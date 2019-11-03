0ecfce7b73ff5ee2bab4d1a83abc0a789ab1b485
knifehand <mhurt@msudenver.edu>
2019-11-02 19:44:30 -0600

    changed some fixtures and attempted adding validation

--- CarParts/app/models/car.rb
+++ CarParts/app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord #FIXME: doesn't like the syntax ::Base
   has_many :products #FIXED: Need to associate the car with the product.
-  validates :vin, numericality: {greater_than_or_equal_to: 1} #FIXED: validations are the cause of the Failures during rails test, CarsControllerTest#test_should_create_car
+  validates :vin, numericality: {greater_than_or_equal_to: 1}#FIXED: validations are the cause of the Failures during rails test, CarsControllerTest#test_should_create_car
   validates :make, presence:true
   validates :model, presence:true
   #validates :year, presence:true #FIXME: "Car.count" didn't change by 1. Expected: 3 Actual: 2
