e092943c488f127004eb3522170afd6ab701ae66
knifehand <mhurt@msudenver.edu>
2019-11-02 22:39:19 -0600

    91% code coverage

--- CarParts/test/test_helper.rb
+++ CarParts/test/test_helper.rb
@@ -1,3 +1,14 @@
+require 'simplecov'
+
+SimpleCov.at_exit do
+  puts SimpleCov.result.format!
+end
+
+SimpleCov.start do
+  add_group "app", "app"
+  add_group "test", "test"
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
