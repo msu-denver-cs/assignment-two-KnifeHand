29f18e3644d260caf46bc58b55dd2ba17eb999ea
knifehand <mhurt@msudenver.edu>
2019-11-05 17:40:44 -0700

    user authentication is a success

--- CarParts/config/routes.rb
+++ CarParts/config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     end
   end
 
-  #root to: "home#index"
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
