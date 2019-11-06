228240d11c1f2021c404d0354083525c6cdcc24a
knifehand <mhurt@msudenver.edu>
2019-11-05 17:42:27 -0700

    user authorization success part 2

---
+++ CarParts/public/javascripts/AJAXSearch.js
@@ -0,0 +1,21 @@
+/*
+$(document).ready(
+    function () {
+        $("#" + QueryTag).keypress(
+            function (event){
+                var current = $("#" + QueryTag + ":text").val();
+                var keypressed = String.fromCharCode(event.which);
+
+                // if the key was a backspace, remove a character
+                if (8 === even,keyCode) {
+                    current = current.substring(0, current.length - 1);
+                }
+                else {
+                    current += String.fromCharCode(event.which);
+                }
+                if (0 === current.length)
+                }
+            }
+        )
+    }
+)*/
